4f1091c5ee821e202682072bcf488a8da9a242a4
Joseph Curley <hey.josephcurley@gmail.com>
2022-01-18 14:45:41 -0500

    Created Facets and URL state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './components/search-bar';
 import ResultObject from './components/result-object';
+import Select from 'react-select';
 import './app.scss';
 
 const searchAPI = 'https://www.metmuseum.org/mothra/collectionlisting/search?';
@@ -15,18 +16,35 @@ const defaultParams = new URLSearchParams({
 });
 
 const defaultParamString = defaultParams.toString();
+const defaultFacetObject = {id: "Facet","options":[]}
 
 const App = () => {
 	const [searchParamsString, setSearchParamsString] = useState(defaultParamString);
 	const [results, setResults] = useState([]);
+	const [facets, setFacets] = useState([defaultFacetObject]);
+
+	const formatAndSetFacets = oldFacets => {
+		oldFacets.shift();
+		const newFacets = oldFacets.map(facet => {
+			const newOptions = facet.values.map(option => {
+				return  {
+					"selected": option.selected,
+					"label": `${option.label} (${option.count})`,
+					"value": option.id
+				}
+			});
+			facet.values = newOptions;
+			return facet;
+		});
+		setFacets(newFacets);
+	};
 
 	const searchCollection = async () => {
-
 		const request = await fetch(`${searchAPI}${searchParamsString}`);
 		const response = await request.json();
 		if (response.results) {
 			setResults(response.results);
-			console.log(response);
+			formatAndSetFacets(response.facets);
 		} else {
 			console.log("No Results");
 		}
@@ -38,8 +56,26 @@ const App = () => {
 		setSearchParamsString(paramsObject.toString());
 	};
 
+	const handleFacetChange= (e, facet) => {
+		const paramsObject = new URLSearchParams(searchParamsString);
+		const newValue = e.map(selectedFacet => selectedFacet.value).join("|")
+		paramsObject.set(facet.id, newValue);
+		setSearchParamsString(paramsObject.toString());
+	};
+
+	useEffect(() => {
+		const url = new URL(`${window.location}`);
+		const params = new URLSearchParams(url.search.slice(1));
+		setSearchParamsString(params.toString());
+	}, []);
+
 	useEffect(() => {
 		searchCollection();
+
+		const params = new URLSearchParams(searchParamsString);
+		params.set('version', 2.0);
+		window.history.replaceState({}, '', `${location.pathname}?${params}`);
+
 	}, [searchParamsString]);
 
 	return (
@@ -49,7 +85,20 @@ const App = () => {
 				onChange={handleSearchQueryChange}
 			/>
 			<section className="cs__facets">
-
+				{facets.map(facet => {
+					return (
+						<Select
+							className="cs__facet"
+							key={facet.id}
+							isMulti
+							isSearchable="true"
+							name={facet.id}
+							placeholder={facet.label}
+							onChange={e => handleFacetChange(e,facet)}
+							options={facet.values}
+						/>
+					);
+				})}
 			</section>
 			<section className="cs__results">
 				{results.map(collectionItem => {
@@ -62,7 +111,6 @@ const App = () => {
 				})}
 			</section>
 			<section className="pagination">
-				TODO: add Pagination.
 			</section>
 		</main>
 	)
