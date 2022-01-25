38e47cdd6f2a2648658acb0be0e64638394c9386
Joseph Curley <hey.josephcurley@gmail.com>
2022-01-25 13:42:22 -0500

    Updated Icons

--- src/components/pagination-controls/index.jsx
+++ src/components/pagination-controls/index.jsx
@@ -9,7 +9,18 @@ const PaginationControls = ({offset, handlePaginationChange, perPage, totalResul
 			className="pagination-button pagination-button--back"
 			onClick={handlePaginationChange}
 			onKeyDown={event => event.key === 'Enter' && handlePaginationChange(event)}>
-			{`<`}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				className="bi bi-chevron-left"
+				viewBox="0 0 16 16">
+				<path
+					fillRule="evenodd"
+					d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+				/>
+			</svg>
 		</button>
 	);
 
@@ -20,7 +31,19 @@ const PaginationControls = ({offset, handlePaginationChange, perPage, totalResul
 			className="pagination-button pagination-button--forward"
 			onClick={handlePaginationChange}
 			onKeyDown={event => event.key === 'Enter' && handlePaginationChange(event)}>
-			{`>`}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				className="bi bi-chevron-left"
+				viewBox="0 0 16 16">
+				<path
+					fillRule="evenodd"
+					d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+				/>
+
+			</svg>
 		</button>
 	);
 
